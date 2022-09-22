5a78f024723b6a03ba57c0f47c71a8aa94909baa
putrisyabillah23 <putrisyabillah5@gmail.com>
2022-09-21 19:20:36 -0700

    Create tugas praktikum algoritma.py

---
+++ tugas praktikum algoritma.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 18 20:10:57 2022
+
+@author: Asus
+"""
+
+print ("hitung luas ruangan")
+p = int(input("panjang ruangan :"))
+l = int(input("lebar ruangan :" ))
+x = (input("satuan :"))
+luas = p*l
+print ("luas ruangan adalah :" + str (luas) + x)
